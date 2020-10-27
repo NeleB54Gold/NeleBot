ceb6a072b1be8bc3f64115e8f8a773ca06088a96
NeleB54Gold <51356760+NeleB54Gold@users.noreply.github.com>
2020-10-27 12:21:48 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,126 +1,91 @@
 <?php
 
 /*
-NeleBotFramework
-	Copyright (C) 2018  PHP-Coders
+    NeleBotWebHookInstaller
+    Copyright (C) 2018  NeleBot Framework
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 # Impostazioni e Configurazioni del Bot
+# Consulta la nostra guida prima di modificare questo file!
+# Guida: https://telegra.ph/NeleBot--PHP-Framework-per-Bot-Telegram-07-20#config.php
 
-$config = [
-	# Configurazione del bot
-	'cloni' => [
-		123456789 => "UsernameBot"
-	],
-	// Metti qui l'ID che indica l'username di ogni Bot (senza @)
-	'password' => false,
-	// Password delle request | Lascia false per disabilitarla oppure scrivi una password
-	'admins' => [
-		244432022 // Nele
-	],
-	// ID degli Amministratori del Bot
-	'devmode' => false,
-	// Developer Mode | Attivala quando il Bot è in fase di testing
-	'method' => 'post',
-	// Scegli get o post come metodo delle request cURL | Uso Default sulle funzioni
-	'response' => false,
-	// Risposta alla request | Uso Default sulle funzioni
-	'request_timeout' => 2,
-	// Timeout dell richiesta
-	'json_payload' => true,
-	// Json Payload è valido solo per le request senza response
-	'usa_il_db' => false,
-	// Decidi se usare il Database | Consigliato l'uso del Database solo dopo i primi test
-	'usa_redis' => false,
-	// Decidi se usare Redis
-	'logs' => false,
-	// Memorizza le updates del tuo Bot (sconsigliato l'utilizzo)
-	'log_report' => [
-		'SHUTDOWN' => true,
-		'FATAL' => true,
-		'ERROR' => true,
-		'WARN' => true,
-		'INFO' => false,
-		'DEBUG' => false
-	],
-	// Segnalazione errori
-	'not_log_report' => [],
-	// Array di Log da non reportare (Esempio: ['redis'])
-	'console' => 244432022,
-	// Inserisci l'ID della chat in cui vuoi mandare i log importanti e gli errori (Cambia quello già inserito) oppure metti false per non utilizzarlo.
-	'whitelist_users' => false,
-	// Blocca gli utenti sconosciuti | Admins auto-inclusi
-	'whitelist_chats' => false,
-	// Blocca le chat sconosciute (gruppi, supergruppi e canali)
-	// false: Whitelist spenta
-	// Array: Whitelist accesa
-	
-	# Impostazioni messaggi
-	'operatori_comandi' => ['/', '!', '.', '>'], 
-	// Utilizzo comandi del Bot (Es: /cmd !cmd .cmd)
-	'post_canali' => true,
-	// Decidi se i comandi funzionano anche per i messaggi dai canali
-	'modificato' => true,
-	// Decidi se i comandi funzionano anche per i messaggi modificati
-	'azioni' => false,
-	// Sta scrivendo... prima di inviare un messaggio | Uso Default sulle funzioni
-	'parse_mode' => "HTML",
-	// Scegli HTML o Markdown | Uso Default sulle funzioni
-	'disabilita_notifica' => false,
-	// Disabilita la notifica dei messaggi | Uso Default sulle funzioni
-	'disabilita_anteprima_link' => true,
-	// Disabilita l'anteprima dei link | Uso Default sulle funzioni
-	
-	'version' => "2.6.4"
-	// Versione di NeleBot
-];
-if (!isset($config['cloni'][$botID])) {
-	if ($config['devmode']) {
-		die("Bot non autorizzato.");
-	} else {
-		header("Content-Type: application/json");
-		echo json_encode(['method' => 'deleteWebhook']);
-		die;
-	}
-} else {
-	$config['username_bot'] = $config['cloni'][$botID];
-}
+$config = array(
+    # Configurazione del bot
+    'username_bot' => "NeleBotWebHookInstallerBot",
+    // Metti qui l'username del Bot (senza @)
+    'password' => false,
+    // Password delle request | Lascia false per disabilitarla oppure scrivi una password
+    'admins' => [244432022, 426538970],
+    // ID degli Amministratori del Bot
+    'devmode' => false,
+    // Developer Mode | Attivala quando il Bot è in fase di testing
+    'method' => 'post',
+    // Scegli get o post come metodo delle request cURL | Uso Default sulle funzioni
+    'response' => false,
+    // Risposta alla request | Uso Default sulle funzioni
+    'json_payload' => false,
+    //Json Payload è valido solo per le request senza response
+    'usa_il_db' => true,
+    // Decidi se usare il Database | Consigliato l'uso del Database solo dopo i primi test
+    'usa_redis' => false,
+    // Decidi se usare Redis
+    'logs' => "ultimo",
+    // Logs delle Update su un file
+    'console' => 24154625,
+    // Inserisci l'ID della chat in cui vuoi mandare i log importanti e gli errori (Cambia quello già inserito) oppure metti false per non utilizzarlo.
+    'whitelist_users' => false,
+    // Blocca gli utenti sconosciuti | Admin auto-inclusi
+    'exec' => false,
+    // Verifica la sintassi dei plugins con la funzione exec
+
+    # Impostazioni messaggi
+    'post_canali' => false,
+    // Decidi se il Bot funziona anche per i messaggi dai canali
+    'modificato' => false,
+    // Decidi se il Bot funziona anche per i messaggi modificati
+    'azioni' => false,
+    // Sta scrivendo... prima di inviare un messaggio | Uso Default sulle funzioni
+    'parse_mode' => "HTML",
+    // Scegli HTML o Markdown | Uso Default sulle funzioni
+    'disabilita_anteprima_link' => true,
+    // Disabilità l'anteprima dei link sui messaggi | Uso Default sulle funzioni
+);
 
 # Dati di accesso al database
-$database = [
-	'type' => "mysql",
-	// Inserisci 'sqlite', 'mysql' o 'postgre' per scegliere il tipo di Database
-	'nome_database' => "",
-	// Nome del database (o nome del file per SQLite)
-	'utente' => "",
-	// Se usi un' altro utente, sostituisci root con il nome.
-	'password' => "",
-	// Password del utente di accesso
-	'host' => "localhost"
-	// IP/dominio del server mysql (lascia invariato se non sai di cosa si tratta)
-];
+$database = array(
+    'type' => "mysql",
+    // Inserisci 'mysql' o 'postgre' per scegliere il tipo di Database
+    'nome_database' => "",
+    // Nome del database
+    'utente' => "",
+    // Se usi un' altro utente, sostituisci root con il nome.
+    'password' => "",
+    // Password del utente di accesso
+    'host' => "localhost"
+    // IP/dominio del server mysql (lascia invariato se non sai di cosa si tratta)
+);
 
-# Dati di accesso a Redis
-$config['redis'] = [
-	'database' => 1,
-	// Database da selezionare per l'uso di Redis. Lascia false per usare root.
-	'host' => "localhost",
-	// Host di connessione al Server Redis.
-	'port' => 6379,
-	// Porta di connessione al Server Redis.
-	'password' => false
-	// Password di autenticazione al Server Redis. Lascia false per disattivarlo.
-];
+//Dati di accesso a Redis
+$config['redis'] = array(
+    'database' => 4745,
+    // Database da selezionare per l'uso di Redis. Lascia false per usare root.
+    'host' => "localhost",
+    // Host di connessione al Server Redis.
+    'port' => 6379,
+    // Porta di connessione al Server Redis.
+    'password' => false,
+    // Password di autenticazione al Server Redis. Lascia false per disattivarlo.
+);
--- plugins/comandi.php
+++ plugins/comandi.php
@@ -1,449 +1,151 @@
 <?php
 
 /*
-NeleBotFramework
-	Copyright (C) 2018  PHP-Coders
+    NeleBotWebHookInstaller
+    Copyright (C) 2018  NeleBot Framework
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-if ($cmd == 'start') {
-	sm($chatID, bold("Bot avviato!") . "\nUsa /help per una lista di comandi.\nℹ️ " . code("Versione {$config['version']}"));
-	die;
+$menustart[0] = array(
+    array(
+        'text' => "➕ Nuovo Bot ➕",
+        'callback_data' => '/newbot'
+    ),
+);
+$menustart[1] = array(
+    array(
+        'text' => "ℹ️ Informazioni ℹ️",
+        'callback_data' => 'stat'
+    ),
+);
+
+if ($cbdata == 'stat') {
+    $testo = "WebHook settati: " . file_get_contents('whs.txt');
+    cb_reply($cbid, $testo, true);
 }
 
-if ($cmd == "type") {
-	db_query("UPDATE utenti SET page = ? WHERE user_id = ?", ["testMessage", $userID]);
-	sm($chatID, "OK, inviami un messaggio e ti dirò che tipo di messaggio è...");
-	die;
-}
-
-if ($cmd == "menu" or $cmd == "mediamenu" or $cbdata == "menu") {
-	$t = "Menu aperto";
-	$menu[] = [
-		[
-			"text" => "ℹ️ Info",
-			"callback_data" => "info"
-		]
-	];
-	if ($cmd == "menu") {
-		sm($chatID, $t, $menu);
-	} elseif ($cmd == "mediamenu") {
-		sp($chatID, "https://telegra.ph/file/5850d536904bb4e8b7dfd.jpg", $t, $menu);
-	} else {
-		if ($caption) {
-			$is_caption = true;
-		} else {
-			$is_caption = false;
-		}
-		cb_reply($cbid, '', false, $cbmid, $t, $menu, 'def', 'def', $is_caption);
-	}
-	die;
-}
-
-if ($cbdata == "info") {
-	$config['json_payload'] = false;
-	$t = bold("Sviluppatore: ") . tag(244432022, "Nele");
-	$t .= bold("\nVersione: ") . code($config['version']);
-	$menu[] = [
-		[
-			"text" => "◀️ Torna indietro",
-			"callback_data" => "menu"
-		]
-	];
-	if ($caption) {
-		$is_caption = true;
-	} else {
-		$is_caption = false;
-	}
-	cb_reply($cbid, '', false, $cbmid, $t, $menu, 'def', 'def', $is_caption);
-	die;
-}
-
-if ($cbdata == "del") {
-	dm($chatID, $cbmid);
-	cb_reply($cbid);
-	die;
-}
-
-if ($cmd == 'cancel') {
-	db_query("UPDATE utenti SET page = ? WHERE user_id = ?", ["", $userID]);
-	sm($chatID, "Comando annullato.", 'nascondi');
-	die;
-}
-
-if ($u['page'] == "testMessage" and $typechat == "private") {
-	sm($chatID, "Tipo di messaggio: $messageType");
-	die;
-}
-
-if ($cmd == "dice") {
-	sdice($chatID, "🏀");
-	sm($chatID, tag() . ", vergognati, mi hai appena chiesto di fare la cosa più inutile di Telegram, fai /help e rendimi utile...");
-	die;
-}
-
-if ($cmd == "tag") {
-	sm($chatID, tag());
-	die;
-}
-
-if ($cmd == "cache") {
-	$config['json_payload'] = false;
-	$propic = getPropic($botID);
-	if (!$propic['ok']) {
-		die;
-	}
-	$file_id = $propic['result']['photos'][0][1]['file_id']; // Qualità 320p
-	copy(getFile($file_id), "cache.jpeg");
-	sm($chatID, "Fatto");
-	die;
-}
-
-if ($cmd == 'documents') {
-	$menu[] = [
-		[
-			"text" => "Da locale",
-			"callback_data" => "doc_locale"
-		],
-		[
-			"text" => "Da link",
-			"callback_data" => "doc_link"
-		],
-		[
-			"text" => "Da file_id",
-			"callback_data" => "doc_file_id"
-		]
-	];
-	sm($chatID, "Documenti", $menu);
-	die;
-}
-
-if (strpos($cbdata, "doc_") === 0) {
-	$tipo = str_replace('doc_', '', $cbdata);
-	$documents = [
-		'locale' => $f['plugins'],
-		'link' => "https://t.me/NeleB54GoldBlog/798",
-		'file_id' => "BQACAgQAAxkDAAIJ2156hyNuwDGlSfF4-3giJkac5aqtAAJsAwAC7YmIU3fnc3N-71esGAQ" // Ogni Bot ha i propri file ID
-	];
-	$config['response'] = true;
-	cb_reply($cbid, '', false);
-	$r = sd($chatID, $documents[$tipo], "test", 'nascondi', 'def', false, true, "cache.jpeg");
-	if (!$r['ok']) {
-		sm($chatID, "Error: " . json_encode($r));
-	}
-	die;
-}
-
-if ($cmd == 'buttons') {
-	$menu[] = [
-		[
-			'text' => "Mostra",
-			'callback_data' => 'mostra'
-		],
-		[
-			'text' => "Mostra URL",
-			'callback_data' => 'mostraurl'
-		]
-	];
-	$menu[] = [
-		[
-			'text' => "Inline",
-			'switch_inline_query_current_chat' => 'messaggio'
-		],
-		[
-			'text' => "Condividi Inline",
-			'switch_inline_query' => 'messaggio'
-		]
-	];
-	$menu[] = [
-		[
-			'text' => "URL",
-			'url' => 'https://t.me/' . $config['username_bot']
-		],
-		[
-			'text' => "Condividi URL",
-			'url' => 'https://t.me/share/url?' . http_build_query([
-				"text" => "Testo da condividere",
-				"url" => "https://t.me/" . $config['username_bot']
-			])
-		]
-	];
-	sm($chatID, "Bottoni", $menu);
-	die;
-}
-
-if ($cbdata == 'mostra') {
-	cb_reply($cbid, 'Ciao', true);
-	die;
-}
-
-if ($cbdata == 'mostraurl') {
-	cb_url($cbid, "https://t.me/" . $config['username_bot'] . "?start=Test");
-	die;
-}
-
-if ($cmd == "start Test") {
-	sm($chatID);
-	die;
+if ($cmd == 'start' and $u['page'] == ' ') {
+    sm($chatID, "<b>Nele WebHook Bot</b>
+Setta il Webhook del tuo Bot", $menustart);
 }
 
 if ($cmd == 'help' and $typechat == "private") {
-	$menu[] = [
-		[
-			'text' => "Funzioni Inline",
-			'switch_inline_query_current_chat' => ''
-		]
-	];
-	$menu[] = [
-		[
-			'text' => "Source Bot",
-			'url' => 't.me/NelePHPFramework'
-		]
-	];
-	$config['json_payload'] = false;
-	$commands = getBotCommands();
-	if (isset($commands['result'])) {
-		foreach ($commands['result'] as $comando) {
-			$comandi .= "\n/{$comando['command']}: {$comando['description']}";
-		}
-	} else {
-		$comandi = italic("\nNessun comando disponibile...");
-	}
-	sm($chatID, bold("Comandi del Bot") . "$comandi\n@" . $config['username_bot'], $menu);
-	die;
+    $menu[0] = array(
+        array(
+            'text' => "Source Bot",
+            'url' => 't.me/NelePHPFramework'
+        ),
+    );
+    sm($chatID, "<b>Webhook Bot</b>
+Questo Bot ti aiuterà a settare il Webhook al tuo Bot con Base NeleBot.
+@" . $config['username_bot'], $menu);
 }
 
-if ($cmd == "setcommands" and $isadmin) {
-	sm($chatID, "OK. Inviami la lista di comandi per questo Bot. Usa questo formato:\n\ncomando1 - Descrizione\ncomando2 - Altra descrizione");
-	db_query("UPDATE utenti SET page = ? WHERE user_id = ?", ['setcommands', $userID], 'no');
-	die;
+if ($cbdata == '/newbot') {
+    $menu[0] = array(
+        array(
+            'text' => "❌ Annulla ❌",
+            'callback_data' => '/cancel'
+        ),
+    );
+    db_query("update utenti set page = 'key' where user_id = $userID");
+    cb_reply($cbid, '✅', false, $cbmid, "<b>Nuovo Bot</b>
+Invia le chiavi API del Bot", $menu);
 }
 
-if ($u['page'] == 'setcommands' and $isadmin) {
-	db_query("UPDATE utenti SET page = ? WHERE user_id = ?", ['', $userID], 'no');
-	$config['json_payload'] = false;
-	$com = explode("\n", $msg);
-	foreach ($com as $ecom) {
-		$e = explode(" - ", $ecom, 2);
-		$commands[] = [
-			'command' => $e[0],
-			'description' => $e[1]
-		];
-	}
-	$s = setBotCommands($commands);
-	if ($s['ok']) {
-		sm($chatID, "✅ Lista comandi aggiornata!");
-	}
-	die;
+if ($msg == '/cancel') {
+    if ($cbdata) {
+        if ($u['page'] !== ' ') {
+            cb_reply($cbid, 'Annullato', false, $cbmid, "<b>Webhook Bot</b> \nSetta il Webhook del tuo Bot",
+                $menustart);
+            db_query("update utenti set page = ' ' where user_id = $userID");
+        } else {
+            cb_reply($cbid, 'Questo comando è già stato annullato', true);
+        }
+    } else {
+        if ($u['page'] !== ' ') {
+            db_query("update utenti set page = ' ' where user_id = $userID");
+            sm($chatID, "Comando cancellato. Usa /start per riavviare.");
+        } else {
+            sm($chatID, "Nessun comando in esecuzione. Usa /start per riavviare.");
+        }
+    }
+    db_query("update utenti set status = '[]' where user_id = $userID");
+    exit;
 }
 
-if ($cmd == "reply") {
-	sm($chatID, "Risposta. \nAdesso rispondi tu.", 'rispondimi', '', $msgID);
-	die;
+if ($msg and $typechat == 'private' and $u['page'] == 'key') {
+    $e = explode(':', $msg);
+    if (strpos($msg, ':') and strlen($msg) < 200 and is_numeric($e[0])) {
+        if (strpos($msg, 'bot') === false) {
+            $msg = 'bot' . $msg;
+        }
+        $r = getMe($msg);
+        if ($r['ok']) {
+            $r = $r['result'];
+            $userbot = $r['username'];
+            $idbot = $r['id'];
+            db_query("update utenti set page = 'password' where user_id = $userID");
+            $u['status']['key'] = $msg;
+            $st = json_encode($u['status']);
+            $query = "update utenti set status = '$st' where user_id = '$userID'";
+            $q = $PDO->prepare($query);
+            $q->execute();
+            $err = $q->errorInfo();
+            if ($err[0] !== "00000") {
+                call_error("PDO Error\n<b>INPUT:</> <code>" . json_encode($q) . "</>\n<b>OUTPUT:</> <code>" . json_encode($err) . "</>");
+            }
+            sm($chatID,
+                "Chiave API Valida! @$userbot [<code>$idbot</code>] \n<b>Adesso invia la password delle request</b>\n<i>Usa</i> /skip <i>se non hai impostato la password</i>");
+        } else {
+            sm($chatID, "Chiave API inesistente");
+        }
+    } else {
+        sm($chatID, "Chiave API non valida, riprova! \nEsempio: 467252147:aihTR1WGKcnCglNEVrMYZJPBQ6k_uqtU3XI");
+    }
 }
 
-if ($cmd == 'jsondump') {
-	sm($chatID, code(substr(json_encode($original_update, JSON_PRETTY_PRINT), 0, 4095)));
-	die;
+if ($msg and $typechat == 'private' and $u['page'] == 'password') {
+    if ($cmd == 'skip') {
+        db_query("update utenti set page = 'webhookurl' where user_id = $userID");
+        sm($chatID, "<b>Ok, password non settata.</b> \nOra inviami il link del Webhook");
+    } else {
+        db_query("update utenti set page = 'webhookurl' where user_id = $userID");
+        $u['status']['password'] = $msg;
+        $st = json_encode($u['status']);
+        $query = "update utenti set status = '$st' where user_id = '$userID'";
+        $q = $PDO->prepare($query);
+        $q->execute();
+        $err = $q->errorInfo();
+        if ($err[0] !== "00000") {
+            call_error("PDO Error\n<b>INPUT:</> <code>" . json_encode($q) . "</>\n<b>OUTPUT:</> <code>" . json_encode($err) . "</>");
+        }
+        sm($chatID, "<b>Ok, password inserita.</b> \nOra inviami il link del Webhook");
+    }
 }
 
-if (in_array($cmd, ['html', 'markdown', 'markdownv2'])) {
-	$config['parse_mode'] = $cmd;
-	$testo = textspecialchars(bold("Bold") . " = ") . bold("Bold") . "\n";
-	$testo .= textspecialchars(italic("Italic") . " = ") . italic("Italic") . "\n";
-	$testo .= textspecialchars(code("Fixed") . " = ") . code("Fixed") . "\n";
-	$testo .= textspecialchars(underl("Underlined") . " = ") . underl("Underlined") . "\n";
-	$testo .= textspecialchars(striket("Strikethrough") . " = ") . striket("Strikethrough") . "\n";
-	$testo .= textspecialchars(text_link("Text Link", 'http://www.example.com') . " = ") . text_link("Text Link", 'http://www.example.com');
-	sm($chatID, $testo);
-	die;
-}
-
-if ($cmd == 'db' and $typechat === "channel") {
-	sm($chatID, code(json_encode($c, JSON_PRETTY_PRINT)));
-	die;
-}
-
-if ($cmd == 'mylocation' and $typechat == "private") {
-	$config['json_payload'] = false;
-	$loc[] = [
-		[
-			"text" => "Invia posizione [Test]", 
-			'request_location' => true
-		]
-	];
-	sm($chatID, "Dove ti trovi?", $loc, 'def', false, false, false);
-	die;
-}
-
-if ($cmd == 'mynumber' and $typechat == "private") {
-	$config['json_payload'] = false;
-	$loc[] = [
-		[
-			"text" => "Invia numero do telefono [Test]", 
-			'request_contact' => true
-		]
-	];
-	sm($chatID, "Come ti chiamo?", $loc, 'def', false, false, false);
-	die;
-}
-
-if ($cmd == 'infome') {
-	if ($config['usa_il_db']) {
-		$db = "\n\n" . bold("Informazioni Utente Database") . "\nNome: " . code($u['nome']) . "\nCognome: " . code($u['cognome']) . "\nUsername: " . code($u['username']) . "\nID: " . code($u['user_id']) . "\nLingua: " . code($u['lang']);
-	}
-	sm($chatID, bold("Informazioni Utente Telegram") . "\nNome: " . code($nome) . "\nCognome: " . code($cognome) . "\nUsername: " . code($username) . "\nID: " . code($userID) . "\nLingua: " . code($lingua) . $db);
-	die;
-}
-
-if ($cmd == 'propic') {
-	$config['json_payload'] = false;
-	$photos = getPropic($userID);
-	if ($photos['ok']) {
-		sp($chatID, $photos['result']['photos'][0][count($photos['result']['photos'][0]) - 1]['file_id'], "Hai " . count($photos['result']['photos']) . " foto profilo.");
-	} else {
-		sm($chatID, bold("Error " . $photos['error_code']) . "\n" . $photos['description']);
-	}
-	die;
-}
-
-if ($cmd == 'contact') {
-	sc($chatID, "+13253080284", "Presidente", "VoIP");
-	die;
-}
-
-if ($cmd == 'editphoto') {
-	$menu[] = [
-		[
-			"text" => "Cambia",
-			"callback_data" => "cbEditPhoto"
-		]
-	];
-	sp($chatID, "https://telegra.ph/file/5850d536904bb4e8b7dfd.jpg", 'cia', $menu);
-	die;
-}
-
-if ($cbdata == "cbEditPhoto") {
-	$menu[] = [
-		[
-			"text" => "Fatto!",
-			"url" => "t.me/NeleB54Gold"
-		]
-	];
-	media_cb_reply($cbid, "Text", true, $cbmid, "t.me/NeleB54Gold", 'photo', "Ciao", $menu);
-	die;
-}
-
-if ($cmd == 'animation') {
-	sgif($chatID, "https://t.me/NeleB54GoldBlog/568", 'Rotola');
-	die;
-}
-
-if ($cmd == 'venue') {
-	sven($chatID, 41.90, 12.50, "Capitale d'Italia", 'Roma');
-	die;
-}
-
-if ($cmd == 'location') {
-	$latitudine = 41.9;
-	$longitudine = 12.4;
-	$menu[] = [
-		[
-			'text' => "⏺",
-			'callback_data' => 'editLocation_' . round($latitudine) . "_" . round($longitudine)
-		]
-	];
-	$menu[] = [
-		[
-			'text' => "Ferma Location",
-			'callback_data' => 'stopLocation'
-		]
-	];
-	sendLocation($chatID, $latitudine, $longitudine, $menu, 7200);
-	die;
-}
-
-if (strpos($cbdata, 'editLocation_') === 0) {
-	$e = explode('_', $cbdata, 3);
-	$lati = $e[1];
-	$long = $e[2];
-	if ($lati > 180) $lati = 180;
-	if ($long > 360) $long = 360;
-	if ($lati < 0) $lati = 0;
-	if ($long < 0) $long = 0;
-	$menu[] = [
-		[
-			'text' => "↖️",
-			'callback_data' => 'editLocation_' . round($lati + 1) . "_" . round($long - 1)
-		],
-		[
-			'text' => "⬆️",
-			'callback_data' => 'editLocation_' . round($lati + 1) . "_" . round($long)
-		],
-		[
-			'text' => "↗️",
-			'callback_data' => 'editLocation_' . round($lati + 1) . "_" . round($long + 1)
-		]
-	];
-	$menu[] = [
-		[
-			'text' => "⬅️",
-			'callback_data' => 'editLocation_' . round($lati) . "_" . round($long - 1)
-		],
-		[
-			'text' => "⏺",
-			'callback_data' => 'editLocation_' . round($lati) . "_" . round($long)
-		],
-		[
-			'text' => "➡️",
-			'callback_data' => 'editLocation_' . round($lati) . "_" . round($long + 1)
-		]
-	];
-	$menu[] = [
-		[
-			'text' => "↙️",
-			'callback_data' => 'editLocation_' . round($lati - 1) . "_" . round($long - 1)
-		],
-		[
-			'text' => "⬇️",
-			'callback_data' => 'editLocation_' . round($lati - 1) . "_" . round($long)
-		],
-		[
-			'text' => "↘️",
-			'callback_data' => 'editLocation_' . round($lati - 1) . "_" . round($long + 1)
-		]
-	];
-	$menu[] = [
-		[
-			'text' => "Ferma Location",
-			'callback_data' => 'stopLocation'
-		]
-	];
-	editLocation($chatID, $lati, $long, $cbmid, $menu);
-	cb_reply($cbid);
-	die;
-}
-
-if ($cbdata == 'stopLocation') {
-	$menu[] = [
-		[
-			'text' => "Developer",
-			'url' => "t.me/NeleB54Gold"
-		]
-	];
-	cb_reply($cbid, 'Posizionamento terminato', true);
-	stopLocation($chatID, $cbmid, $menu);
-	die;
+if ($msg and $typechat == 'private' and $u['page'] == 'webhookurl') {
+    $url = $msg . '?' . http_build_query($u['status']);
+    $r = setWebhook($u['status']['key'], $url);
+    if ($r['ok']) {
+        sm($chatID, "<b>Webhook settato!</b>\n\n<code>" . json_encode($r) . '</code>');
+        db_query("update utenti set page = ' ' where user_id = $userID");
+        db_query("update utenti set status = '[]' where user_id = $userID");
+        file_put_contents('whs.txt', file_get_contents('whs.txt') + 1);
+    } else {
+        sm($chatID,
+            "<b>Impossibile impostare l'URL Webhook</b>\nRiprova o usa /cancel per annullare.\n" . json_encode($r) . "\nIn caso setta tu su <code>$url</>");
+    }
 }
